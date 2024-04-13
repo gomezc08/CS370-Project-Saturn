e7234a0424b06a60257aaf6bcd71f98c5cbec029
Chris G <gomezc@whitman.edu>
2024-04-13 16:20:17 -0700

    creating playlists and adding sounds to database using gui

--- src/gui.py
+++ src/gui.py
@@ -42,14 +42,15 @@ class Gui():
         # Display playlists using radio buttons.
         playlists = self.db.view_playlists()
         self.radio_vars = {}
-        var = StringVar()
+        var = StringVar(value="Small")
 
         def update_label():
             playlist_label.config(text=var.get())
 
         for playlist in playlists:
             self.radio_vars[playlist[0]] = var 
-            Radiobutton(self.playlist, text=playlist[0], variable=var, value=playlist[0], command=update_label, fg="Black", bg=self.bg_color).pack(anchor=W, padx=50, pady = 5)
+            r_button = Radiobutton(self.playlist, text=playlist[0], variable=var, value=playlist[0], command=update_label, fg="Black", bg=self.bg_color)
+            r_button.pack(anchor=W, padx=50, pady = 5)
 
         # click on radio button label.
         playlist_label = Label(self.playlist, text="", fg="Black", bg=self.bg_color)
@@ -63,12 +64,8 @@ class Gui():
         Label(self.playlist, text="Create new Playlist", font=("Arial", 15), padx=50, pady=5, fg="Black", bg=self.bg_color).pack(anchor=E)
         e = Entry(self.playlist, width=50)
         e.pack(anchor=E, padx=50, pady=5)
-
-        def plus_click():
-            entry_value = e.get()
-            print(f"here is the tentry: {entry_value}")
             
-        plus_button = Button(self.playlist, text="+", padx=50, pady=5, fg="Black", bg=self.bg_color, command=plus_click)
+        plus_button = Button(self.playlist, text="+", padx=50, pady=5, fg="Black", bg=self.bg_color, command=lambda: self.db.create_playlist(e.get()))
         plus_button.pack(anchor=E, padx=50, pady=5)
 
         # "Continue" button
@@ -88,8 +85,8 @@ class Gui():
         
         # creating radio buttons for each song in the playlist.
         playlist = self.db.view_sort_playlist(playlist_title)
-        self.radio_vars = {}  
-        var = StringVar()
+        self.radio_vars = {}
+        var = StringVar(value="Small")
         
         def update_label():
             """
@@ -116,6 +113,17 @@ class Gui():
         play_edit = Button(frame_buttons, text="Edit", padx=50, pady = 5, command=self.edit_screen)
         play_edit.grid(row=0, column=1, padx=50, pady = 5)
 
+        # add sound into playlist button IFF the playlist name is not My Library since this playlist by default has every sound.
+        if(playlist_title != "Your Library"):
+            # creating the label.
+            add_l = Entry(self.sound, width=50)
+            add_l.pack(anchor=E, padx=50, pady=5)
+            
+            # creating the button.
+            add_b = Button(self.sound, text="Add to this Playlist", padx=50, pady = 5, bg = "Green", fg = "White", command=lambda: self.db.add_sound_into_playlist(add_l.get(), playlist_title))
+            add_b.pack(padx=50, pady = 5)
+            
+        
         def delete_button():
             """
             This function is bound to the "Delete" button and is called when the button is clicked.
@@ -160,6 +168,14 @@ class Gui():
             button = Button(self.edit, text=button_data["text"], fg="Black", bg=self.bg_color, padx=10, pady=5)
             button.grid(row=button_data["row"], column=button_data["column"], columnspan=button_data["columnspan"], sticky=W)
 
+        # play button.
+        button = Button(self.edit, text="Play", fg="White", bg="Green", padx=30, pady=5)
+        button.grid(row=7, column=1, columnspan=5, sticky=W, pady=20)
+        
+        # save button.
+        button_save = Button(self.edit, text="Save", fg="White", bg="Red", padx=30, pady=5)
+        button_save.grid(row=7, column=20, columnspan=5, sticky=E, pady=20)
+        
 if __name__ == "__main__":
     gui = Gui()
     gui.root.mainloop()
