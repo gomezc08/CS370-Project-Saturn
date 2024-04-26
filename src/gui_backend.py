5e77c1dcbd1f1010894116a8a1e4bb78517f6da7
jeanne <harbingerhvh@gmail.com>
2024-04-26 11:30:11 -0700

    added docstrings

--- src/gui_backend.py
+++ src/gui_backend.py
@@ -9,7 +9,22 @@ from database.PlaylistManager import PlaylistManager
 
 
 class Backend:
+    """
+    Backend class for audio manipulation and playback.
+
+    Attributes:
+        audioSegment (AudioSegment): Original audio segment.
+        changedAudioSegment (AudioSegment): Modified audio segment.
+        reversed (bool): Flag indicating if the audio is reversed.
+    """
+
     def __init__(self, audio_file):
+        """
+        Initializes the Backend with the given audio file.
+
+        Args:
+            audio_file (str): Path to the audio file.
+        """
         # TODO: uncomment this when AudioClustering is fixed
         # clustering = AudioClustering()
         playlistManager = PlaylistManager()
@@ -36,16 +51,34 @@ class Backend:
         self.load_audio(audio_file)
 
     def load_audio(self, audio_file):
+        """
+        Loads the audio file into the Backend.
+
+        Args:
+            audio_file (str): Path to the audio file.
+        """
         self.audioSegment = AudioSegment.from_file(audio_file)
         self.changedAudioSegment = self.audioSegment
 
     def modify_speed(self, change_amnt):
+        """
+        Modifies the playback speed of the audio.
+
+        Args:
+            change_amnt (float): Amount of speed change.
+        """
         # This should work ?
         self.changedAudioSegment = self.changedAudioSegment.speedup(
             playback_speed=change_amnt
         )
 
     def modify_pitch(self, change_amnt):
+        """
+        Modifies the pitch of the audio.
+
+        Args:
+            change_amnt (float): Amount of pitch change.
+        """
         # This should work ?
         self.changedAudioSegment = self.changedAudioSegment.set_frame_rate(
             self.changedAudioSegment.frame_rate * change_amnt
@@ -63,22 +96,53 @@ class Backend:
             self.changedAudioSegment = self.changedAudioSegment.reverse()
 
     def revert(self):
+        """
+        Reverts the audio to its original state.
+        """
         self.changedAudioSegment = self.audioSegment
 
     def play(self):
+        """
+        Plays the modified audio.
+        """
         p.play(self.changedAudioSegment)
 
     def save(self, name):
+        """
+        Saves the modified audio to a file.
+
+        Args:
+            name (str): Name of the output file.
+        """
         self.changedAudioSegment.export(name, format="wav")
 
     def overlap(self, audio_file):
+        """
+        Overlaps the modified audio with another audio file.
+
+        Args:
+            audio_file (AudioSegment): Audio segment to overlap with.
+        """
         self.changedAudioSegment.overlay(audio_file)
 
     def concatentate(self, audio_file, crossfade_value):
+        """
+        Concatenates the modified audio with another audio file.
+
+        Args:
+            audio_file (AudioSegment): Audio segment to concatenate.
+            crossfade_value (int): Crossfade duration in milliseconds.
+        """
         # This should work, adapted straight from saturn
         self.changedAudioSegment.append(audio_file, crossfade=crossfade_value)
 
     def randomInsert(self, audio_file):
+        """
+        Inserts another audio file randomly into the modified audio.
+
+        Args:
+            audio_file (AudioSegment): Audio segment to insert.
+        """
         length = len(self.changedAudioSegment)
 
         # Get random time to insert
